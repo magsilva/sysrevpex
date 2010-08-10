b8b57405b33a38e8c7882347a3c2799d302aa191
paulovich <paulovich@36c42172-3665-4d60-846c-ac75218345ff>
2010-08-10 20:48:00 +0000

    fixing bugs.
    git-svn-id: http://svn.lcad2.icmc.usp.br/svn/sources/trunk/ProjectionExplorer@959 36c42172-3665-4d60-846c-ac75218345ff

--- src/visualizer/projection/distance/DynamicTimeWarping.java
+++ src/visualizer/projection/distance/DynamicTimeWarping.java
@@ -29,28 +29,24 @@ public class DynamicTimeWarping implements Dissimilarity {
         }
 
         //creating the matrix
-        double[][] g = new double[a.length][b.length];
+        if (g == null || g.length < a.length) {
+            g = new float[a.length][b.length];
+        }
 
         //initial condition
-        g[0][0] = Math.sqrt((a[0] - b[0]) * (a[0] - b[0]));
+        g[0][0] = dist(a[0], b[0]);
 
         int r = (int) ((a.length - 1) * WWINDOW);
 
         //filling the diagonals
         for (int i = r + 1, j = 0; i < b.length; i++, j++) {
-            g[j][i] = g[i][j] = Double.POSITIVE_INFINITY;
+            g[j][i] = g[i][j] = Float.POSITIVE_INFINITY;
         }
 
-        //calculate the first row
-        for (int j = 1; j <= r; j++) {
-            double diff = a[j] - b[0];
-            g[0][j] = g[0][j - 1] + Math.sqrt(diff * diff);
-        }
-
-        //calculate the fist column
+        //calculate the first row and column
         for (int i = 1; i <= r; i++) {
-            double diff = a[0] - b[i];
-            g[i][0] = g[i - 1][0] + Math.sqrt(diff * diff);
+            g[0][i] = g[0][i - 1] + dist(a[i], b[0]);
+            g[i][0] = g[i - 1][0] + dist(a[0], b[i]);
         }
 
         //calculate the remaining values
@@ -59,13 +55,18 @@ public class DynamicTimeWarping implements Dissimilarity {
             int max = Math.min(a.length - 1, i + r);
 
             for (int j = min; j <= max; j++) {
-                double diff = a[j] - b[i];
-                g[i][j] = Math.min(Math.min(g[i - 1][j], g[i - 1][j - 1]), g[i][j - 1]) + Math.sqrt(diff * diff);
+                g[i][j] = Math.min(Math.min(g[i - 1][j], g[i - 1][j - 1]), g[i][j - 1]) + dist(a[j], b[i]);
             }
-        }        
+        }
 
         return (float) g[a.length - 1][b.length - 1];
     }
 
+    private float dist(float a, float b) {
+        float diff = a - b;
+        return (float)Math.sqrt(diff * diff);
+    }
+    
+    private static float[][] g;
     private static final float WWINDOW = 0.25f;
 }
